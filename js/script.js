45ece164e986237958a16c9aaa4a2425c4a1cf46
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-05-14 14:08:29 +0200

    example

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// let friends = []//syntax for an array
+
+// function submit(){
+
+//     let Name = document.getElementById("nameText").value;
+//     let Surname = document.getElementById("surnameText").value;
+
+
+//     let friend = {}
+//     friend.firstName = Name;
+//     friend.lastName = Surname;
+
+    
+//     friends.push(friend);  
+//     console.table(friends);
+// }
+let firstName = "Imaraan";
+let lastName = "Sheldon";
+let numberVar = 5;
+// const myName = "Imraan";
+
+// myName = "Shelldon";   ..This gives an error
+
+console.log (firstName + lastName);
+console.log (numberVar * 5);
